bb1a6de95a903533646fce2378ed7e8901a9e669
shimabox <shimabox@gmail.com>
2020-06-13 15:58:10 +0900

    :sparkles: Bind to be used as a facade.

---
+++ src/Facades/LaravelQueryHelper.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaravelQueryAssertion\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static array queryResult(callable $queryCaller)
+ * @method static string format(string $string, string $indentString = '  ')
+ * @method static string highlight(string $string)
+ * @method static string compress(string $string)
+ *
+ * @see \LaravelQueryAssertion\LaravelQueryHelper
+ */
+class LaravelQueryHelper extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'laravelQueryHelper';
+    }
+}
---
+++ src/LaravelQueryAssertionServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaravelQueryAssertion;
+
+use Illuminate\Support\ServiceProvider;
+use LaravelQueryAssertion\Contracts\SqlFormatter as SqlFormatterContract;
+use LaravelQueryAssertion\SqlFormatter\SqlFormatter;
+
+class LaravelQueryAssertionServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->bind(SqlFormatterContract::class, function ($app, $param) {
+            return new SqlFormatter($param[0] ?? null);
+        });
+
+        $this->app->singleton('laravelQueryHelper', function ($app) {
+            return $app->make(
+                LaravelQueryHelper::class,
+                [SqlFormatterContract::class]
+            );
+        });
+    }
+}
