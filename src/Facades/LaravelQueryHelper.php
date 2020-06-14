123856783694e768b68690f2e200c9bf246ce6d1
shimabox <shimabox@gmail.com>
2020-06-14 17:06:31 +0900

    :tada: Add LaravelQueryHelper.

--- src/Facades/LaravelQueryHelper.php
+++ src/Facades/LaravelQueryHelper.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static array queryResult(callable $queryCaller)
+ * @method static array buildedQuery(callable $queryCaller)
  * @method static string format(string $string, string $indentString = '  ')
  * @method static string highlight(string $string)
  * @method static string compress(string $string)
--- src/LaravelQueryHelper.php
+++ src/LaravelQueryHelper.php
@@ -31,6 +31,20 @@ class LaravelQueryHelper
     }
 
     /**
+     * Get the SQL representation of the query after it has been build.
+     *
+     * @param  callable $queryCaller Process to execute the query.
+     * @return array
+     */
+    public function buildedQuery($queryCaller): array
+    {
+        $queryLog = $this->getQueryLog($queryCaller);
+        return array_map(function ($query) {
+            return $this->build($query['query'], $query['bindings']);
+        }, $queryLog);
+    }
+
+    /**
      * Format the whitespace in a SQL string to make it easier to read.
      *
      * @param  string $string       The SQL string
@@ -81,5 +95,63 @@ class LaravelQueryHelper
         DB::disableQueryLog();
 
         return $queryResult;
+    }
+
+    /**
+     * Returns the emulated SQL string
+     *
+     * @param $rawSql
+     * @param $parameters
+     * @return string|string[]|null
+     *
+     * @see https://github.com/panique/pdo-debug/blob/master/pdo-debug.php
+     */
+    private function build($rawSql, $parameters)
+    {
+        $keys   = [];
+        $values = [];
+
+        /*
+         |----------------------------------------------------------------------
+         | Get longest keys first, sot the regex replacement doesn't
+         | cut markers (ex : replace ":username" with "'joe'name"
+         | if we have a param name :user )
+         |----------------------------------------------------------------------
+         */
+        $isNamedMarkers = false;
+        if (count($parameters) && is_string(key($parameters))) {
+            uksort($parameters, function ($k1, $k2) {
+                return strlen($k2) - strlen($k1);
+            });
+            $isNamedMarkers = true;
+        }
+
+        foreach ($parameters as $key => $value) {
+            // check if named parameters (':param') or anonymous parameters ('?') are used
+            if (is_string($key)) {
+                $keys[] = '/:' . ltrim($key, ':') . '/';
+            } else {
+                $keys[] = '/[?]/';
+            }
+
+            // bring parameter into human-readable format
+            if (is_string($value)) {
+                $values[] = "'" . addslashes($value) . "'";
+            } elseif (is_int($value)) {
+                $values[] = (string) $value;
+            } elseif (is_float($value)) {
+                $values[] = (string) $value;
+            } elseif (is_array($value)) {
+                $values[] = implode(',', $value);
+            } elseif (is_null($value)) {
+                $values[] = 'NULL';
+            }
+        }
+
+        if ($isNamedMarkers) {
+            return preg_replace($keys, $values, $rawSql);
+        } else {
+            return preg_replace($keys, $values, $rawSql, 1, $count);
+        }
     }
 }
